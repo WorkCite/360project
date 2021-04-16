233b1e3cb189f9f659ab465b2686a07cebf723e6
wrc9817 <wrc9817@gmail.com>
2021-04-16 17:51:15 +0800

    intro modal

--- 360 proj/js/profile.js
+++ 360 proj/js/profile.js
@@ -22,11 +22,26 @@ btn.on("click", function (event) {
         inner.append(div);
       }
 });*/
-var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+/* intro modal */
+$(function(){
+    var modal = $('.introduction');
+    var intro = $('.intro');
+    var save =$('.save');
+    var cls = $('.cls');
+    intro.on('click',function(){
+        modal.fadeIn(500);
+    });
+    $(window).on('click',function(event){
+        if($(event.target).is(modal)){
+          modal.fadeOut(500);
+        }
+      });
+    cls.on('click',function(){
+        modal.fadeOut(500);
+    });
+    save.on('click',function(){
+        modal.fadeOut(500);
+    });
+    
+});

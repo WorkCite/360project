94c15459eed43644963a8b7a9bfea19d4e639e26
Henter <a936946356@icloud.com>
2021-04-16 23:37:48 -0700

    sql update

--- 360 proj/sql/user.sql
+++ 360 proj/sql/user.sql
@@ -11,3 +11,7 @@ ALTER TABLE `users`
 
 INSERT INTO `users` (`username`, `email`, `password`) VALUES
 ('test', '123456@gmail.com', '0f359740bd1cda994f8b55330c86d845');
+INSERT INTO `users` (`username`, `email`, `password`) VALUES
+('admin', 'admin@qq.com', '21232f297a57a5a743894a0e4a801fc3');
+INSERT INTO `users` (`username`, `email`, `password`) VALUES
+('12341', '12322456@gmail.com', '0f359740bd1cda994f8b55330c86d845');
